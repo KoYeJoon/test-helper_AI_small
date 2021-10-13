4806d85eab45ef2010869a8e0ee858422e2f672d
KoYeJoon <yejoon08@ajou.ac.kr>
2021-10-14 02:37:13 +0900

    hand detection으로 yolo3-tiny 모델 사용 및 api 연결

--- app.py
+++ app.py
@@ -1,24 +1,28 @@
-import requests
-import os
-import uuid
-import json
+from student_identification.detectText import detect_text
+from student_identification.compareFace import compare_faces
+from hand_detection.yolo3.src.yolo3 import YOLO
 
-from detectText import detect_text
-from compareFace import compare_faces
-# from keras.models import load_model
 from dotenv import load_dotenv
 load_dotenv()
 
-from flask import Flask, redirect, url_for, request, render_template
+from flask import Flask, redirect, url_for, request, render_template, send_file
+from flask_cors import CORS
+from keras import backend as K
+
+from PIL import Image
+import cv2
+import numpy as np
+import os
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/',methods=['GET'])
 def index():
     return render_template('index.html')
 
 
-@app.route('/temp/identification',methods=['POST'])
+@app.route('/identification',methods=['POST'])
 def identification():
     test_id = request.form['test_id']
     student_id = request.form['student_id']
@@ -43,21 +47,32 @@ def identification():
     # return {'result' : result_face}
 
 
-@app.route('/temp/detection',methods=['POST'])
+@app.route('/hand-detection',methods=['POST'])
 def detection():
-    pass
-
-
-# @app.route('/identification/face',method=['POST'])
-# def identification():
-#     pass
-
-# @app.route('/tests/{testID}/detection',method=['POST'])
-# def detection():
-#     pass
-
+    # model = yolo.get_model()
+    # model.summary()
+    
+    image = Image.open(request.files['hand_img'])
+    print(yolo)
+    result_image,hand_num,bbox = yolo.detect_image(image)
+    opencvImage = cv2.cvtColor(np.array(result_image), cv2.COLOR_RGB2BGR) 
+    opencvImage = cv2.resize(opencvImage,dsize=(360,240))
+    cv2.imwrite("static/images/result.jpg",opencvImage)
+    result_path = "images/result.jpg"
+    # K.clear_session()
+    # return send_file(result_image, mimetype='image/gif')
+    # return {hand_num : hand_num, bbox : bboxs}
+    return render_template(
+            'result_hand.html', 
+            result_path = result_path,
+            hand_num=hand_num, bbox=bbox
+    )
 
 
 if __name__ == '__main__':
-    # model = load_model('./hand_detection/model.h5')
-    app.run(host='0.0.0.0',port=5000,debug=True)
+    model_path = "hand_detection/yolo3/model_data/custom_weights_final.h5"
+    class_path = "hand_detection/yolo3/model_data/classes.txt"
+    anchor_path = "hand_detection/yolo3/model_data/tiny_yolo_anchor.txt"
+    yolo = YOLO(model_path=model_path, classes_path=class_path, anchors_path=anchor_path)
+    app.run(host='0.0.0.0',port=5000,threaded=True,debug=True)
+    #app.run(host='0.0.0.0',port=5000,debug=True)
