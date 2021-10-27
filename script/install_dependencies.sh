af75d686e0e4d56a1e4e81a38af5c9dd7eb670c2
KoYeJoon <yejoon08@ajou.ac.kr>
2021-10-27 20:33:08 +0900

    aws-cd..도전

---
+++ script/install_dependencies.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd /home/ec2-user/test-helper
+git clone https://github.com/KoYeJoon/test-helper_AI_small.git
---
+++ script/start_server.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd /home/ec2-user/test-helper/test-helper_AI_small
+docker-compose -d up --build
