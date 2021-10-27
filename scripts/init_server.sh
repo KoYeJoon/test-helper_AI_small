01c00672901d2ee200f9dade662e20c4f98e28cb
KoYeJoon <yejoon08@ajou.ac.kr>
2021-10-27 22:17:52 +0900

    code deploy 연결되도록 수정

---
+++ scripts/init_server.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+if [ -d /home/ubuntu/test-helper ]; then
+    rm -rf /home/ubuntu/test-helper 
+fi
+
+mkdir -vp /home/ubuntu/test-helper 
--- scripts/remove_server.sh
+++ scripts/remove_server.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-if [ -d /home/ubuntu/test-helper ]; then
-    rm -rf /home/ubuntu/test-helper 
-fi
-
-mkdir -vp /home/ubuntu/test-helper 
 docker-compose down
